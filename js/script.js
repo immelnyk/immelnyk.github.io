05126a3085e71af9d4b36699e75d5bf5109b6f49
Taras <tarasmelnyk3@gmail.com>
2021-07-13 23:57:53 +0300

    Added some animation

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+gsap.to("#center-windows" ,5, {ease: Power0.easeNone, x: 10 , y: 20, repeat: -1, yoyo: true});
+gsap.to("#left-windows" ,6, {ease: Power0.easeNone, x: 0 , y: 30,rotate: -5, repeat: -1, yoyo: true });
+gsap.to("#right-window" ,4, {ease: Power0.easeNone, x: -10 , y: 30, rotate: 10 ,  repeat: -1, yoyo: true });
+gsap.to("#details" ,10, { x: 20 , y: -10, repeat: -1, yoyo: true , rotate: 10 });
